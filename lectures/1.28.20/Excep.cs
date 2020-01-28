dd17c3aca97aa31525315ed801893c7d7f9bde41
ArtemShnip <tjoma27@gmail.com>
2020-01-28 21:50:35 +0300

    9:50

---
+++ lectures/1.28.20/Excep.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1._28._20
+{
+    class Excep
+    {
+        public void Exc()
+        {
+            string str = null;
+            try
+            {
+                var leng = str?.Length;
+                // throw new Exception("message");
+                //throw new NullReferenceException();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.Source);
+                Console.WriteLine(ex.InnerException);
+
+                throw;
+            }
+            finally
+            {
+                str = null;
+            }
+        }
+    }
+}
---
+++ lectures/1.28.20/Nullable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _1._28._20
+{
+    class Nullable
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int? num = null;
+
+            Nullable<Guid> test = new Guid();
+            Console.WriteLine(test.Value);
+
+            Console.WriteLine(num.GetValueOrDefault(0));
+
+            if (num.HasValue)
+            {
+                Console.WriteLine($"int num = {num.Value}");
+                int num2 = num.Value;
+            }
+            else Console.WriteLine("int num = null");
+
+            int num3 = num.HasValue ? num.Value : 0;
+
+
+        }
+    }
+}
