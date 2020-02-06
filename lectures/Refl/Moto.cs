cad5a6ef6b9750b5f19039697f64ad6c185d358c
ArtemShnip <tjoma27@gmail.com>
2020-02-06 21:50:39 +0300

    ...

---
+++ lectures/Refl/Moto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Refl
+    {
+        class Moto
+        {
+            public string Name { get; set; }
+            public string Model { get; set; }
+
+            private int _vinNumber = 111;
+
+            public int Odometr { get; set; }
+
+        public Moto()
+        {
+            Name = "Def";
+        }
+
+            public Moto(string name)
+            {
+                Name = name;
+            }
+
+            public int GetVinNumber()
+            {
+                return _vinNumber;
+            }
+
+            public override string ToString()
+            {
+                return $"Moto: {Name}, Model: {Model}, VIN: {_vinNumber}";
+            }
+        }
+    }
---
+++ lectures/Refl/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Refl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Moto moto = new Moto("Honda");
+            //moto.GetType();
+            //Console.WriteLine(moto);
+            //using (FileStream f = File.AppendAllLines("",list)) foreach
+            //Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            
+            Type motoType = typeof(Moto);
+
+            Type ctorParametr = Type.GetType("System.String");
+            //ConstructorInfo ctor = motoType.GetTypeInfo().DeclaredConstructors
+               // .FirstOrDefault(ct => ct.GetParameters()[0].ParameterType.Equals(ctorParametr));
+            object[] arg = new object[] { "Honda" };
+            var ctorDefault = motoType.GetConstructors().First();
+
+            //object obj = ctor.Invoke(arg);
+            object obj = ctorDefault.Invoke(null);
+            Console.WriteLine($"Current type - {obj.GetType()}");
+            Console.WriteLine(obj.ToString());
+
+            FieldInfo filedInfo = obj.GetType().GetTypeInfo().GetDeclaredField("_vinNumber");
+            Console.WriteLine($"Filed - {filedInfo.Name}\nIs Private - {filedInfo.IsPrivate}");
+            filedInfo.SetValue(obj, 999_999);
+            Console.WriteLine(obj);
+         }
+    }
+}
